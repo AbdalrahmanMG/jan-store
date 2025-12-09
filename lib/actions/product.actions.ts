f0e46b20b3df5857a4134e031f3addf744123bc5
AbdalrahmanMG <abdalrahman.m.gomaa@gmail.com>
2025-12-09 20:21:04 +0200

    chore: config prisma serverless environment

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,11 +1,10 @@
 "use server"
 
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/db/prisma";
 import { convertToPlanObject } from "../utils";
 import { Env } from "@/config/env";
 
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
 
   const data = await prisma.product.findMany({
     take: Number(Env.LATEST_PRODUCTS_LIMIT) as number,
