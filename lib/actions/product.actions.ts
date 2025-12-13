9f5f2904540b597d9e9c9a2df96573340783a927
AbdalrahmanMG <abdalrahman.m.gomaa@gmail.com>
2025-12-13 15:50:40 +0200

    feat(product): show images in product details page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,15 +1,21 @@
-"use server"
+"use server";
 
 import { prisma } from "@/db/prisma";
 import { convertToPlanObject } from "../utils";
 import { Env } from "@/config/env";
 
 export async function getLatestProducts() {
-
   const data = await prisma.product.findMany({
     take: Number(Env.LATEST_PRODUCTS_LIMIT) as number,
     orderBy: { createdAt: "desc" },
   });
 
   return convertToPlanObject(data);
+}
+
+export async function getProductBySlug(slug: string) {
+  const data = await prisma.product.findFirst({
+    where: { slug: slug },
+  });
+  return data;
 }
